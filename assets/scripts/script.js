1a1da3f1fd4fd1638e51d9800e080e87b37ba510
Sancler Zanella <sanclerzjj@gmail.com>
2021-03-18 20:17:43 +0000

    Add input to the challenge one

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,15 @@
 //Challenge 1 multiplication table function
 //receive a number passed as parameter and return the multiplication table in one column
 
-function multiplication_table(num) {
-    let multiplier = num;
+let button = document.getElementById('calc');
+button.addEventListener('click', function () {
+    let numInput = document.getElementById('multiplicand').value;
+    multiplication_table(numInput);
+});
+
+function multiplication_table(multiplier) {
     let printedResult = document.getElementById('challengeOneResult');
+    printedResult.innerHTML = "";
 
     for (let multiplicand = 1; multiplicand <= 10; multiplicand++) {
         let result = multiplicand * multiplier;
@@ -13,5 +19,3 @@ function multiplication_table(num) {
         printedResult.innerHTML += `${result}<br>`;
     }
 }
-
-multiplication_table(2);
