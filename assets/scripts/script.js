918784d85f3090de834a61e84bb6eb313336dfaf
Sancler Zanella <sanclerzjj@gmail.com>
2021-03-20 15:31:38 +0000

    Add the press enter option to get the result

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,22 @@
 //Challenge 1 multiplication table function
 //receive a number passed as parameter and return the multiplication table in one column
 
-let button = document.getElementById('calc');
-button.addEventListener('click', function () {
-    let numInput = document.getElementById('multiplicand').value;
-    multiplication_table(numInput);
-});
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.getElementById('calc');
+    let mult = document.getElementById('multiplicand');
+    let numInput = mult.value;
+
+    button.addEventListener('click', function () {
+        multiplication_table(numInput);
+    });
+
+    mult.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            multiplication_table(numInput);
+        }
+    });
+
+})
 
 function multiplication_table(multiplier) {
     let printedResult = document.getElementById('challengeOneResult');
@@ -14,8 +25,8 @@ function multiplication_table(multiplier) {
     for (let multiplicand = 1; multiplicand <= 10; multiplicand++) {
         let result = multiplicand * multiplier;
 
-        console.log(result);
+        console.log(`${multiplier} 'x' ${multiplicand} '=' ${result}<br>`);
 
-        printedResult.innerHTML += `${result}<br>`;
+        printedResult.innerHTML += `${multiplier} 'x' ${multiplicand} '=' ${result}<br>`;
     }
 }
