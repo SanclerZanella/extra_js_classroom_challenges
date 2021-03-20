5be542d797fe73c112105ee3f95306b0501434a3
Sancler Zanella <sanclerzjj@gmail.com>
2021-03-20 17:09:53 +0000

    Add challenge two, which returns the largest number between two numbers as parameter

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,17 +4,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     let button = document.getElementById('calc');
     let mult = document.getElementById('multiplicand');
-    let numInput = mult.value;
+    let numInput = Number(mult.value);
 
     button.addEventListener('click', function () {
         multiplication_table(numInput);
-    });
+    })
 
     mult.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             multiplication_table(numInput);
         }
-    });
+    })
 
 })
 
@@ -25,8 +25,38 @@ function multiplication_table(multiplier) {
     for (let multiplicand = 1; multiplicand <= 10; multiplicand++) {
         let result = multiplicand * multiplier;
 
-        console.log(`${multiplier} 'x' ${multiplicand} '=' ${result}<br>`);
+        console.log(`${multiplier} x ${multiplicand} = ${result}<br>`);
 
-        printedResult.innerHTML += `${multiplier} 'x' ${multiplicand} '=' ${result}<br>`;
+        printedResult.innerHTML += `${multiplier} x ${multiplicand} = ${result}<br>`;
+    }
+}
+
+//Challenge 2 max function
+//receive two numbers passed as parameter and return the the largest number
+
+let firstInput = document.getElementById('num1');
+let secondInput = document.getElementById('num2');
+let buttonTwo = document.getElementById('maxMin');
+
+buttonTwo.addEventListener('click', function () {
+    let firstValue = Number(firstInput.value);
+    let secondValue = Number(secondInput.value);
+
+    max(firstValue, secondValue);
+})
+
+
+function max(num1, num2) {
+    let printedResultTwo = document.getElementById('challengeTwoResult');
+
+    if (num1 > num2) {
+        console.log(`the largest number is ${num1}`);
+        printedResultTwo.innerHTML = ` the largest number is ${num1}`;
+    } else if (num2 > num1) {
+        console.log(`the largest number is ${num2}`);
+        printedResultTwo.innerHTML = `the largest number is ${num2}`;
+    } else if (num1 === num2) {
+        console.log(`These numbers are equal`);
+        printedResultTwo.innerHTML = `These numbers are equal`;
     }
 }
