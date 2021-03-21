34f9b285850e69f6f279636026b7b387e64ecbfd
Sancler Zanella <sanclerzjj@gmail.com>
2021-03-21 16:16:18 +0000

    Add the challenge 3 functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,17 +4,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     let button = document.getElementById('calc');
     let mult = document.getElementById('multiplicand');
-    let numInput = Number(mult.value);
 
     button.addEventListener('click', function () {
+        let numInput = Number(mult.value);
         multiplication_table(numInput);
-    })
+    });
 
     mult.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
+            let numInput = Number(mult.value);
             multiplication_table(numInput);
         }
-    })
+    });
 
 })
 
@@ -25,11 +26,12 @@ function multiplication_table(multiplier) {
     for (let multiplicand = 1; multiplicand <= 10; multiplicand++) {
         let result = multiplicand * multiplier;
 
-        console.log(`${multiplier} x ${multiplicand} = ${result}<br>`);
+        console.log(`${multiplier} x ${multiplicand} = ${result}`);
 
         printedResult.innerHTML += `${multiplier} x ${multiplicand} = ${result}<br>`;
     }
 }
+
 
 //Challenge 2 max function
 //receive two numbers passed as parameter and return the the largest number
@@ -45,7 +47,6 @@ buttonTwo.addEventListener('click', function () {
     max(firstValue, secondValue);
 })
 
-
 function max(num1, num2) {
     let printedResultTwo = document.getElementById('challengeTwoResult');
 
@@ -58,5 +59,39 @@ function max(num1, num2) {
     } else if (num1 === num2) {
         console.log(`These numbers are equal`);
         printedResultTwo.innerHTML = `These numbers are equal`;
+    }
+}
+
+///Challenge 3 new_max function
+//receive three numbers passed as parameter and return the the largest number
+
+let firstNum = document.getElementById("numOne");
+let secondNum = document.getElementById("numTwo");
+let thirdNum = document.getElementById("numThree");
+let buttonThree = document.getElementById("maxAnswer");
+
+buttonThree.addEventListener('click', function() {
+    let valueOne = Number(firstNum.value);
+    let valueTwo = Number(secondNum.value);
+    let valueThree = Number(thirdNum.value);
+
+    new_max(valueOne, valueTwo, valueThree);
+});
+
+function new_max(valueOne, valueTwo, valueThree) {
+    let cThreeAnswer = document.getElementById("challengeThreeResult");
+
+    if((valueOne > valueTwo) && (valueOne > valueThree)) {
+        console.log(`The largest number is ${valueOne}`);
+        cThreeAnswer.innerHTML = `The largest number is ${valueOne}`;
+    } else if((valueTwo > valueOne) && (valueTwo > valueThree)) {
+        console.log(`The largest number is ${valueTwo}`);
+        cThreeAnswer.innerHTML = `The largest number is ${valueTwo}`;
+    } else if((valueThree > valueOne) && (valueThree > valueTwo)) {
+        console.log(`The largest number is ${valueThree}`);
+        cThreeAnswer.innerHTML = `The largest number is ${valueThree}`;
+    } else if((valueOne === valueTwo) && (valueTwo === valueThree)) {
+        console.log("These numbers are equal");
+        cThreeAnswer.innerHTML = "These numbers are equal";
     }
 }
